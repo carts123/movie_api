44b5cd25b66fdc998b8796c23ab6c0c75857f6c5
carts123 <72449546+carts123@users.noreply.github.com>
2020-12-18 21:56:35 +0000

    Creation of Express code to route all of the endpoints

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(morgan('common'));
 
 
 // Movie List
-let topMovies = [
+let movies = [
   {
     title: 'The Dark Knight',
     director: 'Christopher Nolan'
@@ -43,11 +43,76 @@ let topMovies = [
 ];
 
 
-//GET Requests
+//Gets the list of movies
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
 });
 
+// Gets the data about a single movie, by title
+app.get('/movies/:title', (req, res) => {
+  res.json(movies.find((movie) =>
+    { return movie.title === req.params.title }));
+});
+// Gets the data about a single genre
+app.get('/movies/genres', (req, res) => {
+  res.json(movies.find((movie) =>
+    { return movie.genre === req.params.genre }));
+});
+// Gets the data about a single director, by name
+app.get('/movies/directors/:name', (req, res) => {
+  res.json(movies.find((movie) =>
+    { return movie.director === req.params.director }));
+});
+
+// Adds data for a new user to register
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+// Update user info (username)
+
+// Adds movie data to their list of favourites
+app.post('/users/:username/movies/:movieID', (req, res) => {
+  let newMovie = req.body;
+
+  if (!newMovie.title) {
+    const message = 'Missing title in request body';
+    res.status(400).send(message);
+  } else {
+    newMovie.id = uuid.v4();
+    movies.push(newMovie);
+    res.status(201).send(newMovie);
+  }
+});
+
+// Removes a movie from the list by ID
+app.delete('/movies/:movieID', (req, res) => {
+  let movie = movies.find((movie) => { return movie.id === req.params.id });
+
+  if (movie) {
+    movies = movies.filter((obj) => { return obj.id !== req.params.id });
+    res.status(201).send('Movie ' + req.params.id + ' was deleted.');
+  }
+});
+
+// Removes a user from the database
+app.delete('/users/:username', (req, res) => {
+  let user = users.find((user => { return user.id === req.params.id });
+
+  if (user) {
+    users = users.filter((obj) => { return obj.id !== req.params.id });
+    res.status(201).send('User ' + req.params.id + ' was deleted.');
+  }
+});
 
 app.use('/', express.static('public'));
 
