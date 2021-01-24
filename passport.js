491856231ca5a64115fb7d25d527d9eb2d3b1754
carts123 <72449546+carts123@users.noreply.github.com>
2021-01-24 21:59:56 +0000

    Syntax errors

--- passport.js
+++ passport.js
@@ -1,11 +1,11 @@
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
-  Models = require('/models.js'),
+  Models = require('./models.js'),
   passportJWT = require('passport-jwt');
 
 let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJWT;
+  ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
   usernameField: 'Username',
