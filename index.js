964af0b2b0ee9ab50f762d7ca119bbdea1a9391a
carts123 <72449546+carts123@users.noreply.github.com>
2021-01-31 12:14:43 +0000

    environmental variables

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB',
+mongoose.connect( process.env.CONNECTION_URI,
 { useNewUrlParser: true, useUnifiedTopology: true });
+
+//mongoose.connect('mongodb://localhost:27017/myFlixDB'
 
 
 app.use(bodyParser.json());
