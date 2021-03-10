3ffbb8fd583860424e4a675e35c11b57f8ea14f5
carts123 <72449546+carts123@users.noreply.github.com>
2021-03-10 18:00:47 +0000

    creation of GET user info endpoint

--- index.js
+++ index.js
@@ -108,6 +108,18 @@ app.get('/director/:Director', passport.authenticate('jwt', { session: false }),
 });
 
 
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Users.findOne({ Username: req.params.Username })
+		.then((user) => {
+			res.json(user);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
+});
+
+
 // Adds data for a new user to register
 app.post('/users',
   [
