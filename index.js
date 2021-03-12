3a44a02efffe704a625f7d157e2d0c8b42463f00
carts123 <72449546+carts123@users.noreply.github.com>
2021-03-12 10:16:41 +0000

    updating cors

--- index.js
+++ index.js
@@ -29,16 +29,7 @@ let auth = require('./auth')(app);
 
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// allows access from all origins app.use(cors());
 
 
 // Get home page
